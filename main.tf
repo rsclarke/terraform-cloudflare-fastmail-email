96a7a9266a348d9742f27869605de576e41d0de5
rsclarke <hey@rsclarke.dev>
2025-04-01 12:39:11 +0800

    Upgrade to cloudflare v5

--- main.tf
+++ main.tf
@@ -1,63 +1,71 @@
-resource "cloudflare_record" "mx1" {
+resource "cloudflare_dns_record" "mx1" {
   zone_id  = var.zone_id
   name     = "@"
   type     = "MX"
-  value    = "in1-smtp.messagingengine.com"
+  content  = "in1-smtp.messagingengine.com"
   priority = 10
+  ttl      = 1
 }
 
-resource "cloudflare_record" "mx2" {
+resource "cloudflare_dns_record" "mx2" {
   zone_id  = var.zone_id
   name     = "@"
   type     = "MX"
-  value    = "in2-smtp.messagingengine.com"
+  content  = "in2-smtp.messagingengine.com"
   priority = 20
+  ttl      = 1
 }
 
-resource "cloudflare_record" "dkim1" {
+resource "cloudflare_dns_record" "dkim1" {
   zone_id = var.zone_id
   name    = "fm1._domainkey"
   type    = "CNAME"
-  value   = "fm1.${var.zone_name}.dkim.fmhosted.com"
+  content = "fm1.${var.zone_name}.dkim.fmhosted.com"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "dkim2" {
+resource "cloudflare_dns_record" "dkim2" {
   zone_id = var.zone_id
   name    = "fm2._domainkey"
   type    = "CNAME"
-  value   = "fm2.${var.zone_name}.dkim.fmhosted.com"
+  content = "fm2.${var.zone_name}.dkim.fmhosted.com"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "dkim3" {
+resource "cloudflare_dns_record" "dkim3" {
   zone_id = var.zone_id
   name    = "fm3._domainkey"
   type    = "CNAME"
-  value   = "fm3.${var.zone_name}.dkim.fmhosted.com"
+  content = "fm3.${var.zone_name}.dkim.fmhosted.com"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "spf" {
+resource "cloudflare_dns_record" "spf" {
   zone_id = var.zone_id
   name    = "@"
-  value   = "v=spf1 ${join(" ", concat(["include:spf.messagingengine.com"], var.spf_terms, ["-all"]))}"
+  content = "v=spf1 ${join(" ", concat(["include:spf.messagingengine.com"], var.spf_terms, ["-all"]))}"
   type    = "TXT"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "dmarc" {
+resource "cloudflare_dns_record" "dmarc" {
   zone_id = var.zone_id
   name    = "_dmarc"
-  value   = "v=DMARC1; p=reject; rua=mailto:${join(",mailto:", var.dmarc_rua)}; ruf=mailto:${join(",mailto:", var.dmarc_ruf)}; fo=1:d:s"
+  content = "v=DMARC1; p=reject; rua=mailto:${join(",mailto:", var.dmarc_rua)}; ruf=mailto:${join(",mailto:", var.dmarc_ruf)}; fo=1:d:s"
   type    = "TXT"
+  ttl     = 1
 }
 
 module "mta_sts" {
   source  = "rsclarke/mta-sts/cloudflare"
-  version = "~> 1.1.0"
+  version = "~>2.0"
 
-  zone_id   = var.zone_id
-  zone_name = var.zone_name
+  account_id = var.account_id
+  zone_id    = var.zone_id
+  zone_name  = var.zone_name
 
   mode    = var.mta_sts_mode
-  mx      = concat([cloudflare_record.mx1.value, cloudflare_record.mx2.value], var.mta_sts_mx)
+  mx      = concat([cloudflare_dns_record.mx1.content, cloudflare_dns_record.mx2.content], var.mta_sts_mx)
   max_age = var.mta_sts_max_age
   rua     = var.tlsrpt_rua
 }
